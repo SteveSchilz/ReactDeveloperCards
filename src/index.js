da93dfe62e5c1feafad52c83afc761db7fa277d1
Steve Schilz <steve.schilz@gmail.com>
2023-09-07 15:30:43 -0700

    Styling Working

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { developers } from "./developers.js";
-
-function Skill(props) {
-  console.log({ props });
-  return (
-    <p>
-      <span>
-        <b>Skills: </b>
-      </span>
-      {props.skills.map((item) => {
-        return <span>{item} </span>;
-      })}
-    </p>
-  );
-}
-
-function Developer(props) {
-  return (
-    <div>
-      <img src={props.devPhoto} alt="Developer"></img>
-      <h2>{props.devName}</h2>
-      <p>{props.devIntro}</p>
-      <Skill skills={props.devSkills} />
-    </div>
-  );
-}
-/*
- * Convention: App (mut be first letter upper case )
- * Our body is a div enclosing a bunch of components
- */
-function App() {
-  console.log(developers);
-  return (
-    <div className="container">
-      <Developer {...developers[0]} />
-      <Developer {...developers[1]} />
-    </div>
-  );
-}
+import "./index.css";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
@@ -46,3 +9,68 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*
+ * Convention: App (mut be first letter upper case )
+ * Our body is a div enclosing a bunch of components
+ */
+function App() {
+  console.log(developers);
+  return (
+    <div className="App">
+      <Developer {...developers[0]} />
+      <Developer {...developers[1]} />
+    </div>
+  );
+}
+
+function Developer(props) {
+  return (
+    <div className="card">
+      <img className="avatar" src={props.devPhoto} alt="Developer"></img>
+      <header className="header">
+        <h1>{props.devName}</h1>
+      </header>
+      <p>{props.devIntro}</p>
+      <Skills skills={props.devSkills} />
+    </div>
+  );
+}
+
+function RandomColor() {
+  var randomColor =
+    "#" + (0x707070 && Math.floor(Math.random() * 0xffffff)).toString(16);
+  return randomColor;
+}
+
+// Generates a random emoji between &#127800; and &#127849
+function RandomEmoji() {
+  var num = 127800 + Math.floor(Math.random() * 50);
+  return <span> {String.fromCodePoint(num)} </span>;
+}
+
+function Skills(props) {
+  console.log({ props });
+  var i = -1;
+  return (
+    <div>
+      <span>
+        <b>Skills:</b>
+      </span>
+      <ul className="skill-list">
+        {props.skills.map((item) => {
+          i = i + 1;
+          return (
+            <li
+              key={i.toString()}
+              className="pill"
+              style={{ backgroundColor: RandomColor() }}
+            >
+              {RandomEmoji()} {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
